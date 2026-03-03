4d9749bb6869f34bb9a6c0b8eaef8ae2f8bd6b65
VikTor <3809164@gmail.com>
2026-03-03 16:12:58 +0200

    Automate superuser creation on Render

--- build.sh
+++ build.sh
@@ -7,3 +7,12 @@ pip install -r requirements.txt
 cd backend
 python manage.py collectstatic --no-input
 python manage.py migrate
+
+# Create superuser if it doesn't exist (using environment variables on Render)
+if [ "$DJANGO_SUPERUSER_USERNAME" ]; then
+  python manage.py createsuperuser \
+    --no-input \
+    --username $DJANGO_SUPERUSER_USERNAME \
+    --email $DJANGO_SUPERUSER_EMAIL \
+    || true
+fi
